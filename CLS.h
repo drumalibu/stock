9bd97e57c32965d82470a229290588cc4f6a85d7
drumalibu <beaverishe@yandex.ru>
2018-06-23 01:29:59 +0300

    commit

--- CLS.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by andrew on 22.06.18.
-//
-
-#ifndef STOCK_CLS_H
-#define STOCK_CLS_H
-
-#endif //STOCK_CLS_H
---
+++ SDL.h
@@ -0,0 +1,22 @@
+//
+// Created by andrew on 22.06.18.
+//
+
+#ifndef STOCK_CLS_H
+#define STOCK_CLS_H
+
+#include <SDL2/SDL.h>
+
+class SDL
+{
+public:
+    createWindow();
+    bmp();
+    texture();
+    clean();
+    
+
+
+};
+
+#endif //STOCK_CLS_H
